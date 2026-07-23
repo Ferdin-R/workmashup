dd35a66af5b9a1067e36d17b136e842c813c5b8e
ferdin <ferdin@gmail.com>
2026-07-23 21:28:45 +0530

    Create retail_analytics database and define initial schema for customers, products, and orders

--- day2work.sql
+++ day2work.sql
@@ -1,1 +1,31 @@
-create database testdb;
+CREATE DATABASE retail_analytics;
+
+SHOW DATABASES;
+
+USE retail_analytics;
+
+CREATE TABLE customers (
+    customer_id INT,
+    full_name VARCHAR(50),
+    gender ENUM('M','F'),
+    city VARCHAR(30),
+    is_active BOOLEAN
+);
+
+CREATE TABLE products (
+    product_id INT,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10,2),
+    manufacture_year YEAR
+);
+
+ALTER TABLE products
+ADD stock_count INT;
+
+CREATE TABLE orders (
+    order_id INT,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10,2),
+);
